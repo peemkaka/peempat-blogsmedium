78ed3c66c63ed3604d951f7a7ae325428e57fa9d
peemkaka <xshot_p@hotmail.com>
2025-05-30 11:49:40 +0700

    [add] Discover button

---
+++ src/components/Common/Posts/Posts.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+<>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    <div>Posts</div>
+    </>
+  )
+}
+
+export default Posts
